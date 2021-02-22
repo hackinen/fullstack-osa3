65b1dc2bf01fdefcb6b2ef7c08450c714479f0f8
Iira <iira.hakkinen@helsinki.fi>
2021-02-22 18:45:55 +0200

    tuotantoversio frontend

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const generateId = () => {
 
 morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 
