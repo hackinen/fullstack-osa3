a10e2fac5161992ffeeaf26f04fc414854e8ec9d
Iira <iira.hakkinen@helsinki.fi>
2021-02-22 18:08:35 +0200

    heroku

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 var morgan = require('morgan')
 const app = express()
 
@@ -9,6 +10,7 @@ const generateId = () => {
 morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
   {
@@ -90,7 +92,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
