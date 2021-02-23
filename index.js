b6d6b48c1b91d880ede69a95ee4eeb211a13e769
Iira <iira.hakkinen@helsinki.fi>
2021-02-23 16:08:35 +0200

    uibuild

--- index.js
+++ index.js
@@ -101,20 +101,8 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (!body.name) {
-      return response.status(400).json({
-        error: 'name missing'
-      })
-    }
-
-    if (!body.number) {
-      return response.status(400).json({
-        error: 'number missing'
-      })
-    }
 
     //nimi ei saa esiintyä kuin kerran
     if (false) {
@@ -131,6 +119,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -138,8 +127,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
